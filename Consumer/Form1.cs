fd0aba1e823df97a2ac9771459e3a7b71783c907
yuragolub21@gmail.com <yuragolub21@gmail.com>
2021-01-03 20:19:58 +0300

    Добавьте файлы проекта.

---
+++ Consumer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Consumer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            var client = new UdpClient(port);
+
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                using (var ms = new MemoryStream(data.Buffer))
+                {
+                    pictureBox1.Image = new Bitmap(ms);
+                }
+                Text = $"Bytes resived: {data.Buffer.Length * sizeof(byte)}";
+            }
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            MessageBox.Show(string.Join("\n", host.AddressList.
+                Where(i => i.AddressFamily == AddressFamily.InterNetwork)));
+        }
+    }
+}
